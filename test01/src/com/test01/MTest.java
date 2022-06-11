08bda0ff7cf60cce0361dd0a16e8aa336370e29e
jeonduksu <jin5114jj@naver.com>
2022-06-11 22:58:42 +0900

    Spring 복습 1일차

---
+++ test01/src/com/test01/MTest.java
@@ -0,0 +1,11 @@
+package com.test01;
+
+public class MTest {
+	public static void main(String[] args) {
+		MessageBean bean = new MessageBeanKo();
+		bean.sayHello("스프링");
+		
+		bean = new MessageBeanEn() ;
+		bean.sayHello("잠와");
+	}
+}
---
+++ test01/src/com/test02/Mtest.java
@@ -0,0 +1,19 @@
+package com.test02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+public class Mtest {
+	
+	public static void main(String[] args) {
+		ApplicationContext factory = new ClassPathXmlApplicationContext("com/test02/Beans.xml");
+		
+		MessageBean bean = (MessageBean)factory.getBean("korea");
+		bean.sayHello("½ºÇÁ¸µ");
+		
+		bean = (MessageBean)factory.getBean("english");
+		bean.sayHello("hello");
+	}
+}
