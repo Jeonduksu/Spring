e615f8b10499fe1b59c6be84ed21f2906e6d64b2
jeonduksu <jin5114jj@naver.com>
2022-06-22 00:42:58 +0900

    Spring 복습 5일차

---
+++ test06/src/test/java/com/boot/hello/Test06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
